2b08f6940a8822875e7497314c38a1dc3b31474c
Matheus Rodrigues <matheussiqueira56@gmail.com>
2021-10-27 22:50:35 -0300

    PUT Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,12 @@ public class CategoryResource {
 					.toUri();
 			
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id ,@RequestBody CategoryDTO dto){
+		dto = service.updateCategory(id,dto);
+		return ResponseEntity.ok().body(dto);
 	}
 	
 }
