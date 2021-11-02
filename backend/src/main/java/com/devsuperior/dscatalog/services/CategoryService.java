d082ecfa78a2757a6f3b3709c5bcf8f2d5f0b6d7
Matheus Rodrigues <matheussiqueira56@gmail.com>
2021-11-02 10:39:46 -0300

    Product entity, SQL Script, Taking out unused imports

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,8 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
