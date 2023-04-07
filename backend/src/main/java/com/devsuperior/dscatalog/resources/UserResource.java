74962ebb7b3b2ea564194b7e1f7361d2f85f99f5
Matheus Rodrigues <matheussiqueira56@gmail.com>
2023-04-07 20:47:50 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -0,0 +1,68 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.services.UserService;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable) {
+		// PARAMETROS: page, size(linesPerPage), sort(orderBy)
+		Page<UserDTO> list = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+		UserDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insertUser(@RequestBody UserInsertDTO dto){
+		UserDTO newDTO = service.insertUser(dto);
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(newDTO.getId())
+					.toUri();
+			
+		return ResponseEntity.created(uri).body(newDTO);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> updateUser(@PathVariable Long id ,@RequestBody UserDTO dto){
+		dto = service.updateUser(id,dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> deleteUser(@PathVariable Long id){
+		 service.deleteUser(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
