d0472bbc95f640d18555ade47a6d9e2b5ee804bf
Matheus Rodrigues <matheussiqueira56@gmail.com>
2023-04-09 16:26:12 -0300

    UserUpdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/RoleRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/RoleRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscatalog.entities.Role;
-import com.devsuperior.dscatalog.entities.User;
 
 /*Camada de Acesso a dados*/
 @Repository
